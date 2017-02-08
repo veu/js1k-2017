eec3806300b84066ef45aa604e573b75004d7a67
Becky <becky@monometric.net>
2017-02-08 14:56:55 +0100

    973 refactor, format, fix windows

--- src/full.js
+++ src/full.js
@@ -1,32 +1,32 @@
-wizard = (x,y) =>
-    y<0 || 47-x<y*2 || y<20-x*3&&y>x*5-4 || x*2>37-y&&x+4>y
+wizard = (x, y) =>
+    y < 0 | 47 - x < y * 2 | y < 20 - x * 3 & y > x * 5 - 4 | x * 2 > 37 - y & x + 4 > y
         ? 0
-        : x+7-y
-            ? x*2>30-y&&x+4>y
+        : x + 7 - y
+            ? x * 2 > 30 - y & x + 4 > y
                 ? 99
-                : 10
+                : 9
             : 30;
 
 mod = x => (x + 360) % 360;
-between = (x,y,z) => x < y && y < z;
+between = (x, y, z) => x < y && y < z;
 
 tower = (x, y) =>
     // door
-    between(18, y, 22) && between(90, x, 94)
+    between(18, y, 22) & between(90, x, 94)
         ? 20
-        : y < 36 && between(84, x, 108)
+        : y < 36 & between(84, x, 108)
             ? 40
             // windows
             : windows[(y / 60 | 0) * 6 + x / 60 | 0] && (
-                between(24, y % 60, 30) && x % 60 < 36
+                between(24, y % 60, 30) & between(0, x % 60, 36)
                     ? 90
-                    : between(30, y % 60, 54) && between(6, x % 60, 30) && 1
+                    : between(30, y % 60, 54) & between(6, x % 60, 30)
             ) || y % 6 && (x + (y / 6 | 0) % 2 * 6) % 12 && 80;
 
 M = Math;
 windows = [];
-for(i=72;i--;)
-    windows[i] = M.random()*12<12-(i/6|0);
+for(i = 72; i--;)
+    windows[i] = M.random() * 12 < 12 - (i / 6 | 0);
 
 scrollx = 1;
 last = 2;
@@ -36,8 +36,8 @@ magic = 180;
 
 keys = [];
 onkeydown = onkeyup = e => {
-    keys[key = e.which-37] = e.type[5];
-    last = key % 2 ? last : key;
+    keys[key = e.which - 37] = e.type[5];
+    last = key % 2 ? last : key
 }
 
 setInterval(e => {
@@ -52,11 +52,11 @@ setInterval(e => {
 
         // update position
         playery += sy;
-        if (playery - scrolly > 120 && sy > 0)
+        if (playery - scrolly > 120 & sy > 0)
             scrolly += sy;
 
         // check collision
-        if (sy < 0 && windows[(playery / 60 | 0) * 6 + mod(98 + scrollx) / 60 | 0] && playery % 60 <= 30 && 30 <= playery % 60 - sy && between(0, 52 - (scrollx + 38) % 60, 52))
+        if (sy < 0 & windows[(playery / 60 | 0) * 6 + mod(98 + scrollx) / 60 | 0] & playery % 60 <= 30 & 30 <= playery % 60 - sy & between(0, 52 - (scrollx + 38) % 60, 52))
             playery += 30 - playery % 30,
             sy = 15;
 
@@ -65,21 +65,21 @@ setInterval(e => {
         // check death
         lost |= scrolly - playery > 30;
 
-        for (x=120;x--;) {
-            a = M.acos(x/60%2-1);
-            xp = (1-a/M.PI)*180|0;
-            for (y=160;y--;)
+        for (x = 120; x--;) {
+            a = M.acos(x / 60 % 2 - 1);
+            xp = (1 - a / M.PI) * 180 | 0;
+            for (y = 160; y--;)
                 color = 
                     // player
-                    between(playery, y+scrolly, playery + 24) && between(82, xp, 98) && wizard(mod(last?xp-83:99-xp),y+scrolly-playery)
+                    between(playery, y + scrolly, playery + 24) & between(82, xp, 98) && wizard(mod(last ? xp - 83 : 99 - xp), y + scrolly - playery)
                     || (scrolly < -y || scrolly + y > 750
                         // sky / ground
                         ? 10
                         // tower
-                        : M.sin(a) * 40 - 60 + tower(mod(xp+scrollx),y+scrolly)
+                        : M.sin(a) * 40 - 60 + tower(mod(xp + scrollx), y + scrolly)
                     ),
-                d = M.min(1, M.max(0,2-M.hypot(60-x, playery-y-scrolly+12)/12)) * magic,
-                c.fillStyle = `hsl(${240+d|0},20%,${color+d/10|0}%)`,
+                d = M.min(1, M.max(0, 2 - M.hypot(60 - x, playery - y - scrolly + 12) / 12)) * magic,
+                c.fillStyle = `hsl(${240 + d | 0},20%,${color + d / 10 | 0}%)`,
                 c.fillRect(x * 4, 640 - y * 4, 4, 4)
         }
     }
