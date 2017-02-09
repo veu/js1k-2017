fe5f78501ffe12de99d4911bc262a75f91143405
Becky <becky@monometric.net>
2017-02-09 22:15:29 +0100

    1029 refactor

--- src/full.js
+++ src/full.js
@@ -40,10 +40,10 @@ setInterval(e => {
     sy = playery > 0 ? sy : 14;
 
     // draw
-    for (x = 120; x--;) {
-        a = M.acos(x / 60 % 2 - 1);
-        xp = (1 - a / M.PI) * 180 | 0;
-        for (y = 160; y--;)
+    for (x = 120; x--;)
+        for (a = M.acos(x / 60 % 2 - 1),
+             xp = (1 - a / M.PI) * 180 | 0,
+             y = 160; y--;)
             color = 
                 // player
                 between(playery, y + scrolly, playery + 24) & between(82, xp, 98) && (
@@ -85,5 +85,4 @@ setInterval(e => {
             d = M.min(1, M.max(0, 2 - M.hypot(60 - x, playery - y - scrolly + 12) / 12)) * magic,
             c.fillStyle = `hsl(${240 + d | 0},20%,${color + d / 6 | 0}%`,
             c.fillRect(x * 4, 640 - y * 4, 4, 4)
-    }
 }, 42)
