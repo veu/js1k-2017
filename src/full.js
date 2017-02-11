5ff93a87b732a492d81d7620a707078cbdcbd31f
Becky <becky@monometric.net>
2017-02-11 23:20:10 +0100

    1015 shorten color values

--- src/full.js
+++ src/full.js
@@ -3,7 +3,7 @@ i60 = 60;
 
 mod = x => (x + i360) % i360;
 between = (x, y, z) => x < y & y < z;
-wall = (x, y) => between(0, y / 6 % 51, 1) ? 70 : y % 6 && (x + (y / 6 & 1) * 6) % 12 && (wall(x + 1, y) ? 80 : i60);
+wall = (x, y) => between(0, y / 6 % 51, 1) ? 7 : y % 6 && (x + (y / 6 & 1) * 6) % 12 && (wall(x + 1, y) ? 8 : 6);
 div60 = x => x / i60 | 0;
 
 windows = [M = Math];
@@ -15,11 +15,11 @@ for (tower = [e = i360 = 360]; e--;)
         tower[e][f] =
             f < 42 & between(126, e, 150)
                 // door
-                ? 40 - e % 3
+                ? 1 + e % 3
                 // windows
                 : windows[div60(f) * 6 + div60(e)] && (
                     between(24, f % i60, i30) & between(0, e % i60, 36)
-                        ? 90
+                        ? 9
                         : between(i30, f % i60, 54) & between(6, e % i60, i30)
                 )
                 // wall
@@ -58,25 +58,25 @@ setInterval(e => {
             for (e = mod(~dir ? x - 53 : 69 - x), y = 160; y--; c.fillRect(x * 4, 640 - y * 4, 4, 4))
                 z = scrolly + y,
                 d = min(1, -min(0, M.hypot(i60 - x, 12 - z + playery) / 12 - 2)) * magic,
-                c.fillStyle = `hsl(${240 + d},25%,${d / 6 + (
+                c.fillStyle = `hsl(${240 + d},25%,${d / 6 + 9 * (
                     // player
                     between(0, f = z - playery + !win * M.sin(x/2)*(14-M.abs(sy))/8|0, 24) & between(52, x, 68) & !(f < 0 | 47 - e < f * 2 | f < 20 - e * 3 & f > e * 5 - 4 | e * 2 > 37 - f & e + 4 > f)
                         ? e + 7 - f
                             ? e * 2 > i30 - f & e + 4 > f
-                                ? 99
-                                : 9
-                            : i30
+                                ? 9
+                                : 1
+                            : 3
                     // world
                     : z < 0
                         // ground
-                        ? 9 + z
+                        ? 2 + z / 6
                         : z > top
                             // sky
                             ? win
-                                ? M.atan2(120 - y, x - i60) * 8 + scrollx/9*M.PI & 1 && 40
-                                : 10
+                                ? M.atan2(120 - y, x - i60) * 8 + scrollx/9*M.PI & 1 && 4
+                                : 1
                             // tower
-                            : M.sin(a = M.acos(x / i60 % 2 - 1)) * 40 - i60 + tower[mod((1 - a / M.PI) * 180 + scrollx | 0)][z]
+                            : M.sin(a = M.acos(x / i60 % 2 - 1)) * 4 - 6 + tower[mod((1 - a / M.PI) * 180 + scrollx | 0)][z]
                 )}%)`
     })(120)
 }, scrollx = 42)
