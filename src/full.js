7379b96eee17b235300035e4581b71cf19a37cf2
Becky <becky@monometric.net>
2017-02-13 12:48:37 +0100

    957 use logical operators over bitwise

--- src/full.js
+++ src/full.js
@@ -1,5 +1,5 @@
 mod = (x, y, z) => (x + 360) % 360,
-between = (x, y, z) => x < y & y < z;
+between = (x, y, z) => x < y && y < z;
 wall = (x, y, z) => between(0, y / 6 % 51, 1) ? 7 : y % 6 && (x + (y / 6 & 1) * 6) % 12 && (wall(x + 1, y) ? 8 : 6);
 div60 = (x, y, z) => x / 60 | 0,
 windowat = (x, y, z) => (z = div60(y) * 6 + div60(x)) * 28 % 64 < 39 - z / 4;
@@ -8,14 +8,14 @@ windowat = (x, y, z) => (z = div60(y) * 6 + div60(x)) * 28 % 64 < 39 - z / 4;
 for (tower = [x = 360]; playery = win = step = sy = x--;) {
     for (tower[x] = [top = y = 1231]; y--;)
         tower[x][y] =
-            y < 42 & between(126, x, magic = 150)
+            y < 42 && between(126, x, magic = 150)
                 // door
                 ? x & 3
                 // windows
                 : windowat(x, y) && (
-                    between(24, y % 60, 30) & between(0, x % 60, 36)
+                    between(24, y % 60, 30) && between(0, x % 60, 36)
                         ? 9
-                        : between(30, y % 60, 54) & between(6, x % 60, 30)
+                        : between(30, y % 60, 54) && between(6, x % 60, 30)
                 )
                 // wall
                 || wall(x, y);
@@ -37,7 +37,7 @@ setInterval(x = (x, y, z) => {
     win = playery > 1228 ? playery = c[sy = 0] = top : 0,
 
     // check window collision
-    (div60(playery) - 19 || step % 60 < 30) && windowat(mod(98 + scrollx), playery) & between(sy, playery % 60 - 30, 1) & between(0, (scrollx + 38) % 60, 52) && (
+    (div60(playery) - 19 || step % 60 < 30) && windowat(mod(98 + scrollx), playery) && between(sy, playery % 60 - 30, 1) && between(0, (scrollx + 38) % 60, 52) && (
         playery += 30 - playery % 60,
         sy = 14
     );
@@ -52,9 +52,9 @@ setInterval(x = (x, y, z) => {
                 d = Math.min(1, -Math.min(0, Math.hypot(60 - x, 12 - z + playery) / 12 - 2)) * magic,
                 c.fillStyle = `hsl(${240 + d},25%,${d / 6 + 9 * (
                     // player
-                    between(0, f = z - playery + !win * Math.sin(x/2)*(14-Math.hypot(sy))/8|0, 24) & between(52, x, 68) & !(f < 0 | 47 - e < f * 2 | f / 3 < 6 - e & f > e * 5 - 4 | e * 2 > 37 - f & e + 4 > f)
+                    between(0, f = z - playery + !win * Math.sin(x/2)*(14-Math.hypot(sy))/8|0, 24) && between(52, x, 68) && !(f < 0 || 47 - e < f * 2 || f / 3 < 6 - e && f > e * 5 - 4 || e * 2 > 37 - f && e + 4 > f)
                         ? e + 7 - f
-                            ? e * 2 > 30 - f & e + 4 > f
+                            ? e * 2 > 30 - f && e + 4 > f
                                 ? 9
                                 : 1
                             : 3
