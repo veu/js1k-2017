7cc1c131664facb570931cb18e6dcc20739b98e7
Becky <becky@monometric.net>
2017-02-14 12:53:49 +0100

    1023 flatten nested loops

--- src/full.js
+++ src/full.js
@@ -1,28 +1,28 @@
+tower = [playery = step = sy = 0];
+scrolly = -12;
+a.style = 'width:480px;height:640px;image-rendering:pixelated';
+
 mod = (x, y, z) => (x + 360) % 360,
 between = (x, y, z) => x < y && y < z;
-wall = (x, y, z) => between(0, y / 6 % 51, 1) ? 7 : y % 6 && (x + (y / 6 & 1) * 6) % 12 && (wall(x + 1, y) ? 8 : 6);
 div60 = (x, y, z) => x / 60 | 0,
 windowat = (x, y, z) => (z = div60(y) * 6 + div60(x)) * 28 % 64 < 39 - z / 4;
+wall = (x, y, z) => between(0, y / 6 % 51, 1) ? 7 : y % 6 && (x + (y / 6 & 1) * 6) % 12 && (wall(x + 1, y) ? 8 : 6);
 
 // precompute tower wall for faster rendering
-for (tower = [x = 360]; playery = step = sy = x--;) {
-    for (tower[x] = [y = 1230]; y--;)
-        tower[x][y] =
-            y < 42 && between(126, x, magic = 150)
-                // door
-                ? x & 3
-                // windows
-                : windowat(x, y) && (
-                    between(24, y % 60, 30) && between(0, x % 60, 36)
-                        ? 9
-                        : between(30, y % 60, 54) && between(6, x % 60, 30)
-                )
-                // wall
-                || wall(x, y);
-    scrolly = -12
-}
-
-a.style = 'width:480px;height:640px;image-rendering:pixelated';
+for (x = 1230 * 360; x--;)
+    y = x / 360 | 0,
+    tower[x] =
+        y < 42 && between(126, mod(x), magic = 150)
+            // door
+            ? mod(x) & 3
+            // windows
+            : windowat(mod(x), y) && (
+                between(24, y % 60, 30) && between(0, mod(x) % 60, 36)
+                    ? 9
+                    : between(30, y % 60, 54) && between(6, mod(x) % 60, 30)
+            )
+            // wall
+            || wall(mod(x), y);
 
 onkeydown = onkeyup = (x, y, z) => c[39 - x.which] = x.type[5];
 
@@ -48,30 +48,32 @@ setInterval(x = (x, y, z) => {
 
     // draw
     (x = (x, y, z) => {
-        for (data = new ImageData(x = 120, 160); x--;)
-            for (e = mod(~dir ? x - 53 : 69 - x), y = 160; y--;)
-                z = scrolly + y,
-                l =
-                    // player
-                    between(0, f = z - playery + !win * Math.sin(x/2)*(14-Math.hypot(sy))/8|0, 24) && between(52, x, 68) && !(f < 0 || 47 - e < f * 2 || f / 3 < 6 - e && e * 5 - 4 < f || 37 - f < e * 2 && f < e + 4)
-                        ? e + 7 - f
-                            ? 30 - f < e * 2 && f < e + 4
-                                ? 7
-                                : 2
-                            : 4
-                    // world
-                    : z < 0
-                        // ground
-                        ? 2 + z / 6
-                        : 1230 < z
-                            // sky
-                            ? win
-                                ? (x - 60) / (100 - y) * 5 + step / 5 & 1 && 4
-                                : 1
-                            // tower
-                            : Math.sin(r = Math.acos(x / 60 % 2 - 1)) * 8 - 6 + tower[mod((1 - r / Math.PI) * 180 + scrollx | 0)][div60(z) - 19 || step % 60 < 30 ? z : z % 12 + 60],
-                    d = Math.min(1, -Math.min(0, Math.hypot(60 - x, 12 - z + playery) / 12 - 2)) * magic * l / 50,
-                    data.data.set([9 * d + l * 8, 6 * d + l * 8, l * 8 + d + l * 8, 255], ((159 - y) * 120 + x) * 4)
+        data = new ImageData(120, 160);
+        for (x = 120 * 160; x--;)
+            y = x / -120 + 159 | 0,
+            e = mod(~dir ? x % 120 - 53 : 69 - x % 120),
+            z = scrolly + y,
+            l =
+                // player
+                between(0, f = z - playery + !win * Math.sin(x % 120/2)*(14-Math.hypot(sy))/8|0, 24) && between(52, x % 120, 68) && !(f < 0 || 47 - e < f * 2 || f / 3 < 6 - e && e * 5 - 4 < f || 37 - f < e * 2 && f < e + 4)
+                    ? e + 7 - f
+                        ? 30 - f < e * 2 && f < e + 4
+                            ? 7
+                            : 2
+                        : 4
+                // world
+                : z < 0
+                    // ground
+                    ? 2 + z / 6
+                    : 1230 < z
+                        // sky
+                        ? win
+                            ? (x % 120 - 60) / (100 - y) * 5 + step / 5 & 1 && 4
+                            : 1
+                        // tower
+                        : Math.sin(r = Math.acos(x % 120 / 60 % 2 - 1)) * 8 - 6 + tower[(div60(z) - 19 || step % 60 < 30 ? z : z % 12 + 60) * 360 + mod((1 - r / Math.PI) * 180 + scrollx | 0)],
+                d = Math.min(1, -Math.min(0, Math.hypot(60 - x % 120, 12 - z + playery) / 12 - 2)) * magic * l / 50,
+                data.data.set([9 * d + l * 8, 6 * d + l * 8, l * 8 + d + l * 8, 255], x * 4);
         c.putImageData(data, 0, 0)
     })(step++)
 }, scrollx = 33)
