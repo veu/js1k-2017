587339780a195203b543c24e9aeb1972fa45bb8a
Becky <becky@monometric.net>
2017-02-15 16:47:34 +0100

    1020 use clamped array

--- src/full.js
+++ src/full.js
@@ -67,7 +67,7 @@ setInterval(x = (x, y, z) => {
                             || f % 6 && (e + (f / 6 & 1) * 6) % 12 && ((1 + e + (f / 6 & 1) * 6) % 12 && 8 || 6)
                         ) * 2 - (g = f * 360 + e) * g / Math.PI % 1 * 2,
                 d = Math.min(1, -Math.min(0, Math.hypot(60 - x % 120, 12 - z + playery) / 12 - 2)) * magic * l / 80,
-                data.data.set([9 * d + l * 8, 6 * d + l * 8, l * 6 + d + l * 8, 255], x * 4);
+                data.data.set([9 * d + l * 8, 6 * d + l * 8, l * 6 + d + l * 8, 360], x * 4);
         c.putImageData(data, 0, 0)
     })(step++)
 }, scrollx = 33)
