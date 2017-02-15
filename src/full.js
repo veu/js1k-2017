fb1b8fc5c4f057cd0466aee48ef61b278b466133
Becky <becky@monometric.net>
2017-02-15 17:57:43 +0100

    1018 use normal form for range checks

--- src/full.js
+++ src/full.js
@@ -1,15 +1,15 @@
 playery = step = sy = scrolly = 0,
 a.style.cssText = 'height:100%;image-rendering:pixelated;image-rendering:-moz-crisp-edges';
 
-mod = (x, y, z) => (x + 360) % 360,
-between = (x, y, z) => x < y && y < z;
-windowat = (x, y, z) => (x = (y / 60 | 0) * 6 + (mod(x) / 60 | 0)) * 28 % 64 < 39 - x / 4;
+mod = (x, y) => (x + 360) % 360,
+between = (x, y) => 0 < x && x < y;
+windowat = (x, y) => (x = (y / 60 | 0) * 6 + (mod(x) / 60 | 0)) * 28 % 64 < 39 - x / 4;
 
 magic = 150,
 
-onkeydown = onkeyup = (x, y, z) => c[39 - x.which] = x.type[5];
+onkeydown = onkeyup = (x, y) => c[39 - x.which] = x.type[5];
 
-setInterval(x = (x, y, z) => {
+setInterval(x = (x, y) => {
     dir = !c[2] - !c[0];
     scrollx = mod(scrollx + dir * 4);
 
@@ -21,7 +21,7 @@ setInterval(x = (x, y, z) => {
     win = 1228 < playery ? playery = c[sy = 0] = 1230 : 0,
 
     // check window collision
-    ((z / 60 | 0) - 19 || step % 60 < 30) && windowat(98 + scrollx, playery) && between(sy, playery % 60 - 30, 1) && between(0, (scrollx + 38) % 60, 52) && (
+    ((z / 60 | 0) - 19 || step % 60 < 30) && windowat(98 + scrollx, playery) && between(playery % 60 - 30 - sy, 1 - sy) && between((scrollx + 38) % 60, 52) && (
         playery += 30 - playery % 60,
         sy = 13
     );
@@ -30,7 +30,7 @@ setInterval(x = (x, y, z) => {
     0 < playery || (sy = 13, playery = -2);
 
     // draw
-    (x = (x, y, z) => {
+    (x = (x, y) => {
         data = new ImageData(120, 160);
         for (x = 120 * 160; x--;)
             y = 160 - (x / 120 | 0),
@@ -38,7 +38,7 @@ setInterval(x = (x, y, z) => {
             z = scrolly + y,
             l =
                 // player
-                between(0, f = z - playery + !win * Math.sin(x % 120/2)*(14-Math.hypot(sy))/8|0, 24) && between(52, x % 120, 68) && !(f < 0 || 47 - e < f * 2 || f < 18 - e * 3 && e * 5 - 4 < f || 37 - f < e * 2 && f < e + 4)
+                between(f = z - playery + !win * Math.sin(x % 120/2)*(14-Math.hypot(sy))/8|0, 24) && between(x % 120 - 52, 16) && !(f < 0 || 47 - e < f * 2 || f < 18 - e * 3 && e * 5 - 4 < f || 37 - f < e * 2 && f < e + 4)
                     ? e + 7 - f
                         ? 30 - f < e * 2 && f < e + 4
                             ? 18
@@ -58,12 +58,12 @@ setInterval(x = (x, y, z) => {
                             e = mod((1 - r / Math.PI) * 180 + scrollx | 0),
                             f = (z / 60 | 0) - 19 || step % 60 < 30 ? z : z % 12 + 60,
                             // door
-                            f < 42 && between(126, e, 150) && e % 3 + 1
+                            f < 42 && between(e - 126, 24) && e % 3 + 1
                             // windows
-                            || windowat(e, f) && between(24, f % 60, 30) && between(0, e % 60, 36) && 10
-                            || windowat(e, f) && between(30, f % 60, 54) && between(6, e % 60, 30)
+                            || windowat(e, f) && between(f % 60 - 24, 6) && between(e % 60, 36) && 10
+                            || windowat(e, f) && between(f % 60 - 30, 24) && between(e % 60 - 6, 24)
                             // wall
-                            || between(0, f / 6 % 51, 1) && 7
+                            || between(f / 6 % 51, 1) && 7
                             || f % 6 && (e + (f / 6 & 1) * 6) % 12 && ((1 + e + (f / 6 & 1) * 6) % 12 && 8 || 6)
                         ) * 2 - (g = f * 360 + e) * g / Math.PI % 1 * 2,
                 d = Math.min(1, -Math.min(0, Math.hypot(60 - x % 120, 12 - z + playery) / 12 - 2)) * magic * l / 80,
